6e4c125547d36b7a876cea48694bff0fab9b0713
Shahjalal Hossain <shahjalal.tipu@gmail.com>
2018-12-25 17:49:53 +0600

    initial setup

--- src/App.js
+++ src/App.js
@@ -6,20 +6,14 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
+        <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
+          <h2>Welcome to React</h2>
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+          
+        </div>
       </div>
     );
   }
